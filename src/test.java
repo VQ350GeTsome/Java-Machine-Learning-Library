635a115901b74966a1bf13d42d7438228428fcf0
VQ350GeTsome <harrison102636@gmail.com>
2025-12-21 13:38:20 -0500

    Updated the test for a more rigorous test

--- src/test.java
+++ src/test.java
@@ -1,39 +1,53 @@
-
-
-
 public class test {
 
-    // XOR test
     public static void main(String[] args) {
         
-        AIML.MLP mlp = new AIML.MLP(0.1f, 2, 4, 1);
+        AIML.MLP mlp = new AIML.MLP(0.05f, 4, 8, 6, 1);
         
-        mlp.setAllLayerActivation(AIML.Activation.HARD_SIGMOID);
-        mlp.setFinalLayerActivation(AIML.Activation.LINEAR);
+        mlp.setAllLayerActivation(AIML.Activation.TANH);
+        mlp.setFinalLayerActivation(AIML.Activation.SIGMOID);
         
-        float [][] inputs = {
-            { 0 , 0 },
-            { 1 , 0 },
-            { 0 , 1 },
-            { 1 , 1 }
+        float[][] inputs = {
+            {0,0,0,0},
+            {0,0,0,1},
+            {0,0,1,0},
+            {0,0,1,1},
+            {0,1,0,0},
+            {0,1,0,1},
+            {0,1,1,0},
+            {0,1,1,1},
+            {1,0,0,0},
+            {1,0,0,1},
+            {1,0,1,0},
+            {1,0,1,1},
+            {1,1,0,0},
+            {1,1,0,1},
+            {1,1,1,0},
+            {1,1,1,1}
         };
-        
+
         float[][] targets = { 
-            { 1 }, 
-            { 0 },
-            { 0 },
-            { 1 }
+            {0},{1},{1},{0}, 
+            {1},{0},{0},{1}, 
+            {1},{0},{0},{1}, 
+            {0},{1},{1},{0} 
         };
         
         // Train
-        for (int e = 0; 5000000 > e; e++) 
-            mlp.train(inputs[e % 4], targets[e % 4]);
+        for (int e = 0; 1000000 > e; e++) {
+            int i = e % inputs.length; 
+            mlp.train(inputs[i], targets[i]);
+        }
         
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; inputs.length > i; i++) {
             float[] out = mlp.forward(inputs[i]);
+            float output = Math.round(out[0]);
+            float target = targets[i][0];
             System.out.println(
-                    "Input: " + java.util.Arrays.toString(inputs[i]) + "\tOutput: " + java.util.Arrays.toString(out) + 
-                    "\tTarget: " + java.util.Arrays.toString(targets[i])
+                "Input: " + java.util.Arrays.toString(inputs[i]) + "\t" + 
+                "Output: " + output +"\t" + 
+                "Target: " + target + "\t" +
+                "Conclusion? " + ((target == output)?"Correct":"Wrong")
             );
         }
     }
